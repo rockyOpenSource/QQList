86bd5815dd3753fea582ace59f5c53b2aa5f5b95
李俊峰 <13816147439@163.com>
2016-02-28 22:22:04 +0800

    Initial Commit

---
+++ JFList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFList
+//
+//  Created by 李俊峰 on 16/2/28.
+//  Copyright © 2016年 李俊峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
